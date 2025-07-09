5dd7f5cf95faa9132bc867d1a2c7816063e5cd9c
aashik2005 <aashik3905@gmail.com>
2025-07-09 17:35:27 +0530

    Time: 5 ms (56.50%) | Memory: 43.1 MB (82.32%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int size1=s.length();
+        int size2=t.length();
+        int arr1[]=new int[26];
+        int arr2[]=new int[26];
+        if(size1!=size2){
+            return false;
+        }
+        for(int i=0;i<size1;i++){
+            int ind1=s.charAt(i)-'a';
+            int ind2=t.charAt(i)-'a';
+            arr1[ind1]+=1;
+            arr2[ind2]+=1;
+        }
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
