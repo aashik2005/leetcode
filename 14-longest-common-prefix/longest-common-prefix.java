26cebc8c3c579f6c36b28deb80cf09ad9fcbb200
aashik2005 <aashik3905@gmail.com>
2025-07-22 19:28:38 +0530

    Time: 7 ms (13.13%) | Memory: 42.4 MB (7.90%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,22 +1,37 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        // String ans="";
+        // boolean flag=true;
+        // boolean kick=true;
+        // for(int i=0;i<strs[0].length();i++){
+        //     if(!flag){
+        //         break;
+        //     }
+        //     char check=strs[0].charAt(i);
+        //     flag=true;
+        //     for(int j=1;j<strs.length;j++){
+        //         if(strs[j].length()-1<i || strs[j].charAt(i)!=check){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag)
+        //     ans+=check;
+        // }
+        // return ans;
+        Arrays.sort(strs);
+        String s1=strs[0];
+        String s2=strs[strs.length-1];
         String ans="";
-        boolean flag=true;
-        boolean kick=true;
-        for(int i=0;i<strs[0].length();i++){
-            if(!flag){
+        int i=0;
+        while(i<s1.length()){
+            if(s1.charAt(i)==s2.charAt(i)){
+                ans+=s1.charAt(i);
+                i++;
+            }
+            else{
                 break;
             }
-            char check=strs[0].charAt(i);
-            flag=true;
-            for(int j=1;j<strs.length;j++){
-                if(strs[j].length()-1<i || strs[j].charAt(i)!=check){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
-            ans+=check;
         }
         return ans;
     }
