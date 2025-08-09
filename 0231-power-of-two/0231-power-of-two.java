e48f06643b889890fafbb31fecd92bf437f9192b
aashik2005 <aashik3905@gmail.com>
2025-08-09 17:07:15 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.72%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isPowerOfTwo(long n) {
-        if(n==1){
-            return true;
-        }
-        else if(n==0){
-            return false;
-        }
-        else{
-            while(n%2==0){
-                n/=2;
-            }
-            if(n==1){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        // if (n==0)
-        // return false;
-        // else 
-        // return (n&(n-1))==0;
+        // if(n==1){
+        //     return true;
+        // }
+        // else if(n==0){
+        //     return false;
+        // }
+        // else{
+        //     while(n%2==0){
+        //         n/=2;
+        //     }
+        //     if(n==1){
+        //         return true;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        if (n==0)
+        return false;
+        else 
+        return (n&(n-1))==0;
     }
 }
