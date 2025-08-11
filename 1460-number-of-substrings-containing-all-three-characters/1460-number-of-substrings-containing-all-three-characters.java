e88fda7f7d1b5414485f5700c7cb52481d35eecf
aashik2005 <aashik3905@gmail.com>
2025-08-12 00:14:23 +0530

    Time: 12 ms (73.68%), Space: 45.1 MB (21.61%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,16 +1,22 @@
 class Solution {
-    private static boolean check(HashMap<Character,Integer> map,char ch){
-        map.put(ch,map.get(ch)-1);
-        if(map.get(ch)==0){
-            map.remove(ch);
+    private static boolean check(int hash[],int ind){
+        hash[ind]-=1;
+        if(hash[ind]==0){
             return false;
         }
         return true;
+        // map.put(ch,map.get(ch)-1);
+        // if(map.get(ch)==0){
+        //     map.remove(ch);
+        //     return false;
+        // }
+        // return true;
     }
     public int numberOfSubstrings(String s) {
         int left=0;
         int len=s.length();
-        HashMap<Character,Integer> map=new HashMap<>();
+        //HashMap<Character,Integer> map=new HashMap<>();
+        int[] hash=new int[3];
         boolean a=false;
         boolean b=false;
         boolean c=false;
@@ -18,26 +24,30 @@ class Solution {
         for(int right=0;right<len;right++){
             if(s.charAt(right)=='a'){
                 a=true;
-                map.put('a',map.getOrDefault('a',0)+1);
+                hash[0]+=1;
+                //map.put('a',map.getOrDefault('a',0)+1);
             }
             else if(s.charAt(right)=='b'){
                 b=true;
-                map.put('b',map.getOrDefault('b',0)+1);
+                hash[1]+=1;
+                //map.put('b',map.getOrDefault('b',0)+1);
             }
             else if(s.charAt(right)=='c'){
                 c=true;
-                map.put('c',map.getOrDefault('c',0)+1);
+                hash[2]+=1;
+                //map.put('c',map.getOrDefault('c',0)+1);
             }
             while(a && b && c){
                 count+=(len-right);
                 if(s.charAt(left)=='a'){
-                    a=check(map,'a');
+                    //a=check(map,'a');
+                    a=check(hash,0);
                 }
                 else if(s.charAt(left)=='b'){
-                    b=check(map,'b');
+                    b=check(hash,1);
                 }
                 else if(s.charAt(left)=='c'){
-                    c=check(map,'c');
+                    c=check(hash,2);
                 }
                 left++;
             }
