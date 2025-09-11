0d4bbe8ff4ef65f41aec403b5755cd27e3387388
aashik2005 <aashik3905@gmail.com>
2025-09-12 00:02:48 +0530

    Time: 1 ms (99.62%) | Memory: 44.3 MB (78.96%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int R=board.length;
-        int C = board[0].length;
+        int C=board[0].length;
         int size=9;
-        int [] rowf=new int [R];
-        int [] colf=new int[C];
-        int [] subarrf=new int[size];
-        int Sas=3;
+        int rowf[]=new int[R];
+        int colf[]=new int[C];
+        int subarr[]=new int[size];
         for(int row=0;row<R;row++){
             for(int col=0;col<C;col++){
+                int subindex=3*(row/3)+(col/3);
                 if(board[row][col]!='.'){
                     int digit=board[row][col]-'0';
-                    int subindex=Sas*(row/Sas)+(col/Sas);
-                    if(((rowf[row]&(1<<digit))!=0) || ((colf[col]&(1<<digit))!=0) || ((subarrf[subindex]&(1<<digit))!=0)){
+                    if((rowf[row]&(1<<digit))!=0 || (colf[col]&(1<<digit))!=0 || (subarr[subindex]&(1<<digit))!=0){
                         return false;
                     }
-                    rowf[row] |=(1<<digit);
-                    colf[col] |=(1<<digit);
-                    subarrf[subindex] |=(1<<digit);
+                    rowf[row]|=1<<digit;
+                    colf[col]|=1<<digit;
+                    subarr[subindex]|=1<<digit;
                 }
             }
         }
