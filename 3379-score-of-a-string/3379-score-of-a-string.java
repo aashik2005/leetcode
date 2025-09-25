7963c4c5a86300b46a672a631449f2331f028a77
aashik2005 <aashik3905@gmail.com>
2025-09-25 19:39:57 +0530

    Time: 1 ms (99.53%), Space: 42.3 MB (16.09%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int ans=0;
+        for(int i=0;i<s.length()-1;i++){
+            int a=(int)s.charAt(i);
+            int b=(int)s.charAt(i+1);
+            ans+=Math.abs(a-b);
+        }
+        return ans;
+    }
+}
