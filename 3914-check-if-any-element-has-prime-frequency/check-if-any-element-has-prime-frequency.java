36a3177c4f4399f0a4d6fbecb1d6ab374ccdfe05
aashik2005 <aashik3905@gmail.com>
2025-11-15 23:20:36 +0530

    Time: 1 ms (97.64%) | Memory: 44.3 MB (12.23%) - LeetSync

---
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.java
@@ -0,0 +1,23 @@
+class Solution {
+    private static boolean prime(int val){
+        for(int i=2;i<val-1;i++){
+            if(val%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkPrimeFrequency(int[] nums) {
+        int freq[]=new int[101];
+        for(int n:nums){
+            freq[n]++;
+        }
+
+        for(int i=0;i<101;i++){
+            if(freq[i]>1 && prime(freq[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
