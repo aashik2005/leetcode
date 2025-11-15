58ba5ec0cac2cd37742826ce32f0cc002b3b9f1d
aashik2005 <aashik3905@gmail.com>
2025-11-15 23:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (5.90%) - LeetSync

--- 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.java
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.java
@@ -1,9 +1,5 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int l=nums.length;
-        if(l==1){
-            return 0;
-        }
         int a=nums[0];
         for(int n:nums){
             if(a!=n){
