db8f9423697fd872bd455033e7f595c4198007bc
aashik2005 <aashik3905@gmail.com>
2025-12-13 08:59:19 +0530

    Time: 3 ms (73.16%), Space: 50.4 MB (9.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -11,7 +11,7 @@ class Solution {
 
         for(int row=1;row<R;row++){
             for(int col=1;col<C;col++){
-                grid[row][col]=Math.min(grid[row][col]+grid[row-1][col],grid[row][col]+grid[row][col-1]);
+                grid[row][col]+=Math.min(grid[row-1][col],grid[row][col-1]);
             }
         }
         return grid[R-1][C-1];
