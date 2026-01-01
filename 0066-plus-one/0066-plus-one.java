da211db5baf1aeaa4c7a9da5f26484100dc27232
aashik2005 <aashik3905@gmail.com>
2026-01-01 13:47:43 +0530

    Time: 0 ms (100%), Space: 43.5 MB (19.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,5 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        // long num=0;
-        // for(int i=0;i<digits.length;i++){
-        //     num=num*10+(long)digits[i];
-        // }
-        // num++;
-        // String st=Integer.toString((int)num);
-        // int len=st.length();
-        // int arr[]=new int[len];
-        // long rem;
-        // for(int i=len-1;i>=0;i--){
-        //     rem=num%10;
-        //     arr[i]=(int)rem;
-        //     num=num/10;
-        // }
-        // return arr;
         int len=digits.length-1;
         for(int i=len;i>=0;i--){
             if(digits[i]!=9){
