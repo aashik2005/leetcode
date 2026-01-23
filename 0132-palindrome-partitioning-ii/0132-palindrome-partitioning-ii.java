65aab192c2df0732947cd8605e9df518e0ba9023
aashik2005 <aashik3905@gmail.com>
2026-01-23 14:15:52 +0530

    Time: 33 ms (70.29%), Space: 47.6 MB (46.77%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-    private static boolean ispallindrome(String str){
-        int i=0;
-        int j=str.length()-1;
-        if(i==j){
-            return true;
-        }
-        while(i<j){
-            if(str.charAt(i)!=str.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
     public int minCut(String s) {
         int n=s.length();
         int dp[]=new int[n+1];
+        boolean ispal[][]=new boolean[n][n];
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(s.charAt(i)==s.charAt(j)){
+                    if(len<=2 || ispal[i+1][j-1]){
+                        ispal[i][j]=true;
+                    }
+                }
+            }
+        }
         for(int i=n-1;i>=0;i--){
             int mincost=Integer.MAX_VALUE;
             for(int j=i;j<n;j++){
-                if(ispallindrome(s.substring(i,j+1))){
+                if(ispal[i][j]){
                     int cost=dp[j+1]+1;
                     mincost=Math.min(cost,mincost);
                 }
