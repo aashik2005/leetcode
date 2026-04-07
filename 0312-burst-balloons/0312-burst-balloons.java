f314d0d3f4b7b18df19102ad4ace26bdc1bea751
aashik2005 <aashik3905@gmail.com>
2026-04-07 08:38:26 +0530

    Time: 106 ms (43.08%), Space: 44.4 MB (72.53%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,25 +1,32 @@
 class Solution {
+    int[][] dp;
+    int[] val;
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n + 2];
-        arr[0] = 1;
-        arr[n + 1] = 1;
-        for (int i = 0; i < n; i++) {
-            arr[i + 1] = nums[i];
+        val = new int[n + 2];
+        val[0] = 1;
+        val[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
         }
-        int[][] dp = new int[n + 2][n + 2];
+        dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return recurse(1, n);   
+    }
+    private int recurse(int i, int j) {  
+        if (i > j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        int best = 0;
+        for (int k = i; k <= j; k++) {
+            int coins =
+                val[i - 1] * val[k] * val[j + 1]    
+                + recurse(i, k - 1)                 
+                + recurse(k + 1, j);                 
 
-        for (int len = 2; len < n + 2; len++) {
-            for (int left = 0; left + len < n + 2; left++) {
-                int right = left + len;
-                for (int k = left + 1; k < right; k++) {
-                    dp[left][right] = Math.max(
-                        dp[left][right],
-                        dp[left][k] + dp[k][right] + arr[left] * arr[k] * arr[right]
-                    );
-                }
-            }
+            best = Math.max(best, coins);
         }
-        return dp[0][n + 1];
+        return dp[i][j] = best;
     }
 }
