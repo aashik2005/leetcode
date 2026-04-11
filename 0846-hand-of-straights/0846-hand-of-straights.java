1d00126226d8dfb980eba6ea0af2142dda8e9dfc
aashik2005 <aashik3905@gmail.com>
2026-04-11 12:50:27 +0530

    Time: 58 ms (44.2%), Space: 48 MB (63.89%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -2,38 +2,61 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         int N=hand.length;
         if(N % groupSize!=0)return false;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        PriorityQueue<Integer> heap=new PriorityQueue<>();
-        for(int i=0;i<N;i++){
-            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        //using treeMap
+        TreeMap<Integer,Integer> card=new TreeMap<>();
+        for(int n:hand){
+            card.put(n,card.getOrDefault(n,0)+1);
         }
-        for(int key:map.keySet()){
-            heap.offer(key);
-        }
-        Stack<Integer> st=new Stack<>();
-        while(!heap.isEmpty()){
-            List<Integer> arr=new ArrayList<>();
+        while(!card.isEmpty()){
+            int curr=card.firstKey();
             for(int i=0;i<groupSize;i++){
-                if(heap.isEmpty()) return false;
-                int val=heap.poll();
-                map.put(val,map.get(val)-1);
-                if(arr.size()==0 || arr.get(arr.size()-1)==val-1){
-                    arr.add(val);
-                    st.push(val);
-                }
-                else{
+                if(!card.containsKey(curr)){
                     return false;
                 }
-            }
-            while(!st.isEmpty()){
-                if(map.get(st.peek())>0){
-                    heap.add(st.pop());
+                int cnt=card.get(curr);
+                if(cnt==1){
+                    card.remove(curr);
                 }
                 else{
-                    st.pop();
+                    card.put(curr,cnt-1);
                 }
+                curr+=1;
             }
         }
         return true;
+
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // PriorityQueue<Integer> heap=new PriorityQueue<>();
+        // for(int i=0;i<N;i++){
+        //     map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        // }
+        // for(int key:map.keySet()){
+        //     heap.offer(key);
+        // }
+        // Stack<Integer> st=new Stack<>();
+        // while(!heap.isEmpty()){
+        //     List<Integer> arr=new ArrayList<>();
+        //     for(int i=0;i<groupSize;i++){
+        //         if(heap.isEmpty()) return false;
+        //         int val=heap.poll();
+        //         map.put(val,map.get(val)-1);
+        //         if(arr.size()==0 || arr.get(arr.size()-1)==val-1){
+        //             arr.add(val);
+        //             st.push(val);
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        //     while(!st.isEmpty()){
+        //         if(map.get(st.peek())>0){
+        //             heap.add(st.pop());
+        //         }
+        //         else{
+        //             st.pop();
+        //         }
+        //     }
+        // }
+        // return true;
     }
 }
